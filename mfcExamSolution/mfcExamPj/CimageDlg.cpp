f8234294d1d95f512bb10d0e4e5da36980c0cb94
LeeSooBin96 <a9622880@naver.com>
2024-03-17 09:19:37 +0900

    Final Ver1.
    1. 1200*800 White 이미지 만들기
    2. x1, y1 좌표 입력 받아 반지름이 r인 원 그리기
    3. (x1,y1)~(x2,y2)까지 일정 간격으로 이동하기 & 이미지 저장
    4. 랜덤한 이미지 로드 & 원의 중앙 좌표값 출력하기

---
+++ mfcExamSolution/mfcExamPj/CimageDlg.cpp
@@ -0,0 +1,144 @@
+﻿// CimageDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "mfcExamPj.h"
+#include "afxdialogex.h"
+#include "CimageDlg.h"
+
+
+// CimageDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CimageDlg, CDialogEx)
+
+CimageDlg::CimageDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CimageDlg, pParent)
+{
+
+}
+
+CimageDlg::~CimageDlg()
+{
+}
+
+void CimageDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CimageDlg, CDialogEx)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+// CimageDlg 메시지 처리기
+BOOL CimageDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+	initImage();
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+void CimageDlg::initImage() //이미지 초기화 메소드
+{
+	int nWidth = 1200; //이미지 폭
+	int nHeight = 800; //이미지 높이
+	int nBpp = 8; //그레이 레벨 픽셀 사용(0~255)
+
+	m_image.Create(nWidth, -nHeight, nBpp); //이미지 생성
+	if (nBpp == 8)
+	{
+		//흑백으로 하기 위한 설정
+		static RGBQUAD rgb[256];
+		for (int i = 0; i < 256; i++)
+		{
+			rgb[i].rgbRed = rgb[i].rgbGreen = rgb[i].rgbBlue = i;
+		}
+		m_image.SetColorTable(0, 256, rgb);
+	}
+
+	//이미지 포인터 가져오기(바이트 배열의 첫요소의 주소를 받아온다)
+	unsigned char* fm = (unsigned char*)m_image.GetBits();
+	memset(fm, 0xff, nWidth * nHeight); //White로 설정
+}
+
+
+
+void CimageDlg::OnPaint() //화면이 갱신될 필요가 있을때 자동 호출
+{
+	CPaintDC dc(this); // device context for painting
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	if (m_image) {
+		m_image.Draw(dc, 0, 0);
+	}
+	// 그리기 메시지에 대해서는 CDialogEx::OnPaint()을(를) 호출하지 마십시오.
+}
+
+void CimageDlg::drawCircle(int nCenX1, int nCenY1, int nRadius)
+{
+	int nWidth = m_image.GetWidth();
+	int nHeight = m_image.GetHeight();
+	int nPitch = m_image.GetPitch();
+
+	unsigned char* fm = (unsigned char*)m_image.GetBits(); //이미지 포인터 가져오기
+	memset(fm, 0xff, nWidth * nHeight);
+	for (int j = nCenY1-nRadius;j < nCenY1 + nRadius;j++) {
+		for (int i = nCenX1-nRadius;i < nCenX1 + nRadius;i++) {
+			if(isInArea(i,j)&&isInCircle(i,j,nCenX1,nCenY1,nRadius)) //영역 안이고, 원 조건에 맞으면
+				fm[j * nPitch + i] = 0; //검은색으로 그리자
+		}
+	}
+
+	//이미지 저장해야한다. (파일명: nCenX1bynCenY1.bmp)
+	CString strFileName;
+	strFileName.Format(_T("..//Image/%dby%d.bmp"), nCenX1, nCenY1);
+	m_image.Save(strFileName); //프로젝트 파일의 Image폴더 안에 저장
+
+	updateDisplay();
+}
+
+bool CimageDlg::isInArea(int i, int j)
+{
+	int nWidth = m_image.GetWidth();
+	int nHeight = m_image.GetHeight();
+
+	CRect rect(0, 0, nWidth, nHeight); //영역 생성
+	return rect.PtInRect(CPoint(i, j)); //영역 안에 있는지 검사
+}
+
+bool CimageDlg::isInCircle(int i, int j, int nCenterX, int nCenterY, int nRadius)
+{
+	bool bRet = false;
+
+	double dX = i - nCenterX;
+	double dY = j - nCenterY;
+	double dDist = dX * dX + dY * dY;
+	//거리벡터값이 반지름보다 작거나 같으면
+	if (dDist <= nRadius * nRadius) bRet = true; 
+
+	return bRet;
+}
+
+void CimageDlg::loadImage(int nCenterX, int nCenterY)
+{
+	if (m_image != NULL) m_image.Destroy(); //기존 이미지 제거
+	CString strFileName;
+	strFileName.Format(_T("..//Image/%dby%d.bmp"), nCenterX, nCenterY);
+	m_image.Load(strFileName);
+
+	updateDisplay();
+	m_image.Destroy(); //로드한 이미지 제거
+	initImage(); //이미지 다시 초기화 -- 이 두 단계있어야 여러번 돌려도 안터짐!
+}
+
+void CimageDlg::updateDisplay()
+{
+	//화면 업데이트
+	CClientDC dc(this);
+	m_image.Draw(dc, 0, 0);
+}
---
+++ mfcExamSolution/mfcExamPj/CimageDlg.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CimageDlg 대화 상자
+
+class CimageDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CimageDlg)
+
+private:
+	CImage m_image; //이미지를 그리기 위한 객체 생성
+
+public:
+	CimageDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CimageDlg();
+
+	//필요한 함수(메서드)
+	void drawCircle(int nX1,int nY1,int nRadius);
+	void loadImage(int nCenterX,int nCenterY);
+
+private:
+	void initImage(); //이미지 초기화 메소드
+	void updateDisplay(); //이미지창 갱신
+	//영역 안인지 검사
+	bool isInArea(int i, int j);
+	//원 안에 있는지 조건 검사
+	bool isInCircle(int i, int j, int nCenterX, int nCenterY, int nRadius);
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CimageDlg };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+};
