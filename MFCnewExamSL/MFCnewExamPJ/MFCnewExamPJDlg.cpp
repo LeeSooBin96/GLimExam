02296ad9be52003a26e699df68210b13ec4b78fc
LeeSooBin96 <a9622880@naver.com>
2024-04-02 08:36:52 +0900

    추가과제 완료
    과제 수행 내용
    1. 다이얼로그에 이미지를 생성
    2. 다이얼로그에 (x1,y1),(x2,y2) 입력창 생성
    3. draw 버튼 클릭시 (x1,y1) 좌표에 원 생성
    4. action 버튼 클릭시 (x1,y1)에서 (x2,y2)로 일정간격으로 원 이동
    5. 일정간격(프레임)마다 이미지를 프로젝트 폴더 내 image폴더에 저장
    6. open 버튼 클릭시 새 다이얼로그에 저장된 이미지 목록이 나옴
    7. 목록 중 하나의 이미지를 로드할 시 콘솔에 원의 중앙 좌표를 출력, 이미지에도 좌표값 X로 표기

---
+++ MFCnewExamSL/MFCnewExamPJ/MFCnewExamPJDlg.cpp
@@ -0,0 +1,266 @@
+﻿
+// MFCnewExamPJDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCnewExamPJ.h"
+#include "MFCnewExamPJDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#pragma comment(linker,"/entry:wWinMainCRTStartup /subsystem:console")
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCnewExamPJDlg 대화 상자
+
+
+
+CMFCnewExamPJDlg::CMFCnewExamPJDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCNEWEXAMPJ_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCnewExamPJDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCnewExamPJDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_DESTROY()
+	ON_BN_CLICKED(IDC_BTN_DRAW, &CMFCnewExamPJDlg::OnBnClickedBtnDraw)
+	ON_BN_CLICKED(IDC_BTN_ACTION, &CMFCnewExamPJDlg::OnBnClickedBtnAction)
+	ON_BN_CLICKED(IDC_BTN_OPEN, &CMFCnewExamPJDlg::OnBnClickedBtnOpen)
+END_MESSAGE_MAP()
+
+
+// CMFCnewExamPJDlg 메시지 처리기
+
+BOOL CMFCnewExamPJDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	MoveWindow(50, 50, 800, 560);
+	m_pImgDlg = new CimageDlg; //new 연산자 사용시 delete 잊지 말기
+	m_pImgDlg->Create(IDD_CimageDlg, this); //ID와 부모 다이얼로그 등록
+	m_pImgDlg->ShowWindow(SW_SHOW); //모달리스트 대화창 생성
+	m_pImgDlg->MoveWindow(20, 20, 650, 490); //창 이동 및 크기조절
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMFCnewExamPJDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCnewExamPJDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCnewExamPJDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFCnewExamPJDlg::OnDestroy()
+{
+	CDialogEx::OnDestroy();
+
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	if (m_pImgDlg != NULL) delete m_pImgDlg;
+}
+
+void CMFCnewExamPJDlg::OnBnClickedBtnDraw()
+{
+	//draw 버튼 클릭 시
+	srand(time(NULL)); //랜덤 시드값 초기화
+
+	int nX1 = GetDlgItemInt(IDC_EDIT_X1);
+	int nY1 = GetDlgItemInt(IDC_EDIT_Y1);
+	if (!isValidX(nX1) || !isValidY(nY1)) {
+		AfxMessageBox(_T("좌표 값이 범위를 벗어났습니다. \n 0<X1<640 ,0<Y1<480"));
+		return;
+	}
+	int nRadius = rand()%50+1; //원의 반지름 랜덤 지정 (1~ 50)
+	//원이 영역 안에 있을 수 있는 반지름  찾기
+	while(!isCenterInArea(nX1,nY1,nRadius)) nRadius = rand() % 50 + 1;
+	m_pImgDlg->m_nRadius = nRadius; //생성된 반지름 저장
+	m_pImgDlg->m_nIMGNum = 0; //생성된 이미지 수 초기화
+	
+	m_pImgDlg->drawCircle(nX1, nY1);
+	m_bDraw = true;
+}
+
+void CMFCnewExamPJDlg::OnBnClickedBtnAction()
+{
+	if (!m_bDraw) {
+		AfxMessageBox(_T("draw 버튼을 먼저 눌러주세요."));
+		return;
+	}
+	//action 버튼 클릭시
+	int nX1 = GetDlgItemInt(IDC_EDIT_X1);
+	int nY1 = GetDlgItemInt(IDC_EDIT_Y1);
+	int nX2 = GetDlgItemInt(IDC_EDIT_X2);
+	int nY2 = GetDlgItemInt(IDC_EDIT_Y2);
+	if (!isValidX(nX1) || !isValidY(nY1) || !isValidX(nX2) || !isValidY(nY2)) {
+		AfxMessageBox(_T("좌표 값이 범위를 벗어났습니다. \n 0<X<640 ,0<Y<480"));
+		return;
+	}
+	//이동 간격
+	int nTermX = (nX1 < nX2) ? 10 : -10;
+	int nTermY = (nY1 < nY2) ? 10 : -10;
+	//원 이동
+	for (int i = nX1, j = nY1;;) {
+		i += nTermX; j += nTermY; //이동
+		if ((nTermX > 0) ? i > nX2:i < nX2) i = nX2; //범위 검사
+		if ((nTermY > 0) ? j > nY2:j < nY2) j = nY2;
+		if (isCenterInArea(i, j, m_pImgDlg->m_nRadius))
+			m_pImgDlg->drawCircle(i, j); //원 그리기
+		if (i == nX2 && j == nY2) break; //목적지에 도착하면 중지
+		m_pImgDlg->saveCircleIMG(); //파일 이미지 저장
+		Sleep(10);
+	}
+
+	m_bDraw = false; 
+}
+
+//원의 중심이 이미지 영역 내에 있는지 검사
+bool CMFCnewExamPJDlg::isCenterInArea(int x, int y, int radius)
+{
+	int nWidth = m_pImgDlg->m_image.GetWidth();
+	int nHeight = m_pImgDlg->m_image.GetHeight();
+	
+	//검사 영역
+	CRect rect(radius-1, radius-1, nWidth - radius+1, nHeight - radius+1);
+	return rect.PtInRect(CPoint(x, y)); //영역 내만 TRUE 경계도 FALSE
+}
+
+//유효한 x좌표 값인지 검사
+bool CMFCnewExamPJDlg::isValidX(int x)
+{
+	return x >0  && x < m_pImgDlg->m_image.GetWidth();
+}
+
+//유효한 y좌표 값인지 검사
+bool CMFCnewExamPJDlg::isValidY(int y)
+{
+	return y > 0 && y < m_pImgDlg->m_image.GetHeight();
+}
+
+
+
+void CMFCnewExamPJDlg::OnBnClickedBtnOpen()
+{
+	//open 버튼 클릭시
+	CString strFileDir;
+
+	CFileDialog fileDlg(TRUE, _T(".bmp"),NULL,0,L"파일선택(*.bmp)|*.bmp;");
+
+	if (fileDlg.DoModal() == IDOK)
+	{
+		strFileDir = fileDlg.GetPathName(); //경로 받기
+		m_pImgDlg->loadCircleIMG(&strFileDir);
+	}
+}
---
+++ MFCnewExamSL/MFCnewExamPJ/MFCnewExamPJDlg.h
@@ -0,0 +1,49 @@
+﻿
+// MFCnewExamPJDlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include "CimageDlg.h"
+// CMFCnewExamPJDlg 대화 상자
+class CMFCnewExamPJDlg : public CDialogEx
+{
+	CimageDlg* m_pImgDlg; //동적으로 생성하기 위한 대화창 포인터
+	bool m_bDraw = false; //draw 버튼 클릭 여부 확인
+	
+	//유효한 x좌표 값인지 검사
+	bool isValidX(int x);
+	//유효한 y좌표 값인지 검사
+	bool isValidY(int y);
+	//원의 중심이 이미지 영역 내에 있는지 검사
+	bool isCenterInArea(int x, int y, int radius);
+
+// 생성입니다.
+public:
+	CMFCnewExamPJDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCNEWEXAMPJ_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnDestroy();
+	afx_msg void OnBnClickedBtnDraw();
+	afx_msg void OnBnClickedBtnAction();
+	afx_msg void OnBnClickedBtnOpen();
+};
